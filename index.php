6b9d73eff5ebd8face74e9d3f08b05f8ef0edb5f
Eman Sulaeman <emanuser13@gmail.com>
2019-06-28 14:41:52 +0700

    Komit fix

---
+++ index.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eman Sulaeman | Impressive Developer</title>
+	<link rel="icon" href="01.jpg">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<script type="text/javascript">
+	var nama = 'eman';
+	console.log(nama);
+</script>
+
+<div class="jumbotron">
+	<div class="overlay"></div>
+		<div class="container">
+			<div class="content">
+			<h1 style="color: white;"><b>Hello, I am</b></h1><br>
+				<p>Eman Sulaeman</p>
+			<div class="impressive">IMPRESSIVE DEVELOPER</div>
+		</div>
+	</div>
+</div>
+<main>
+<div class="container">
+	<div class="row">
+		<div class="span-12">
+			<div class="profile">
+				<p class="nama"><b>Eman Sulaeman</b></p>
+				<h5 style="font-family: 'Open Sans', sans-serif;letter-spacing: 5px;font-weight: 900;">WEB DEVELOPER</h5>
+				<p class="desc" style="margin-top: 25px; letter-spacing: 1px;">I'm a web developer with a lot of experience. Create a website with choice of any programming language and framework. My job is to analyze, design, develop, solve problems, and document the applications created. I'm also a create with other developers to build a quality website. In addition, I also play a role in analyzing the business of the website built.</p><br><br>
+				<p class="signature">Eman Sulaeman</p>
+			</div>
+		</div>
+		<div class="span-12">
+			<div class="kontak">
+				<p><b>Nick Name : </b>Eman</p>
+				<p><b>Email : </b>emanuser13@gmail.com</p>
+				<p><b>Phone : </b>+6285883680147</p>
+				<p><b>Date Of Birth : </b>May 13</p>
+				<p><b>Address : </b>Lebak, Banten, Indonesia</p>
+				<div class="row" style="font-size: 18px; text-align: center;">
+					<div class="span-2"><p style="margin-top: 30px;font-size: 45px;color:#F7941D"><b>8</b></p><div class="caption">Project Completed</div></div>
+					<div class="span-2"><p style="margin-top: 30px;font-size: 45px;color:#873E95"><b>2</b></p><div class="caption">Winning Awards</div></div>
+					<div class="span-2"><p style="margin-top: 30px;font-size: 45px;color:#3C70B4"><b>2</b></p><div class="caption">Happy Clients</div></div>
+					<div class="span-2"><p style="margin-top: 30px;font-size: 45px;color:#87C05E"><b>1</b></p><div class="caption">Running Project</div></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<div style="background-color: #F9F9F9; padding: 30px 0;">
+		<div class="container">
+			<div class="row">
+				<div class="span-12">
+					<p class="skill">My Skills</p>
+					<h3>Programming Skill</h3>
+					<p class="desc" style="padding: 10px 0;">I really like to explore about programming. Therefore, I have mastered several programming languages and frameworks.</p><br>
+				</div>
+				<div class="span-6">
+					<div class="bar-left">PHP</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+					<div class="bar-left">JavaScript</div>
+						<div class="bar-right">75%</div><br>
+							<div class="breadcrumb">
+						<div class="skills intermediate"></div>
+					</div><br><br>
+					<div class="bar-left">Laravel</div>
+						<div class="bar-right">55%</div><br>
+							<div class="breadcrumb">
+						<div class="skills newbie"></div>
+					</div><br><br>
+					<div class="bar-left">Github</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+
+				</div>
+				<div class="span-6">
+					<div class="bar-left">SQL</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+						
+					<div class="bar-left">Java</div>
+						<div class="bar-right">55%</div><br>
+							<div class="breadcrumb">
+							<div class="skills newbie"></div>
+						</div><br><br>
+					<div class="bar-left">MVC</div>
+						<div class="bar-right">75%</div><br>
+							<div class="breadcrumb">
+							<div class="skills intermediate"></div>
+						</div><br><br>
+					<div class="bar-left">Codeigniter</div>
+						<div class="bar-right">55%</div><br>
+							<div class="breadcrumb">
+							<div class="skills newbie"></div>
+						</div><br><br>
+				</div>
+				<div class="span-12">
+					<h3 style="margin-top: 30px;">Software Skill</h3>
+					<p class="desc" style="padding: 10px 0;">Most software I master autodidact.</p><br>
+				</div>
+				<div class="span-6">
+					<div class="bar-left">Word</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+					<div class="bar-left">Excel</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+					<div class="bar-left">Web Browser DevTool</div>
+						<div class="bar-right">75%</div><br>
+							<div class="breadcrumb">
+						<div class="skills intermediate"></div>
+					</div><br><br>
+				</div>
+				<div class="span-6">
+					<div class="bar-left">Power Point</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+					<div class="bar-left">Access</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+					<div class="bar-left">Photoshop</div>
+						<div class="bar-right">75%</div><br>
+							<div class="breadcrumb">
+						<div class="skills intermediate"></div>
+					</div><br><br>
+				</div>
+				<div class="span-12">
+					<h3 style="margin-top: 30px;">Hardware, OS & Networking Skill</h3>
+					<p class="desc" style="padding: 10px 0;">I gained knowledge about the hardware and network at school.</p><br>
+				</div>
+				<div class="span-6">
+					<div class="bar-left">Troubleshooting</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+					<div class="bar-left">Linux</div>
+						<div class="bar-right">75%</div><br>
+							<div class="breadcrumb">
+						<div class="skills intermediate"></div>
+					</div><br><br>
+				</div>
+				<div class="span-6">
+					<div class="bar-left">Mikrotik</div>
+						<div class="bar-right">75%</div><br>
+							<div class="breadcrumb">
+						<div class="skills intermediate"></div>
+					</div><br><br>
+					<div class="bar-left">Installing OS</div>
+						<div class="bar-right">90%</div><br>
+							<div class="breadcrumb">
+						<div class="skills excellent"></div>
+					</div><br><br>
+				</div>
+				<div class="span-12">
+					<h3 style="margin-top: 30px;">Language Skill</h3>
+					<p class="desc" style="padding: 10px 0;">In addition to programming languages, I also studied foreign languages namely English and Arabic. English for my career and Arabic for my religion.</p><br>
+				</div>
+				<center>
+					<div class="span-2">
+						<div class="bullet intermediate"><div class="desc2">70%</div></div>
+						<p>English</p><br><br>
+					</div>
+					<div class="span-2">
+						<div class="bullet newbie"><div class="desc2">50%</div></div>
+						<p>Arabic</p><br><br>
+					</div>
+					<div class="span-2">
+						<div class="bullet top"><div class="desc2">100%</div></div>
+						<p>Indonesia</p><br><br>
+					</div>
+					<div class="span-2">
+						<div class="bullet intermediate"><div class="desc2">80%</div></div>
+						<p>Sunda</p><br><br>
+					</div>
+				</center>
+			</div>
+		</div>
+	</div>
+	<div class="span-12"> 	
+		<div class="container">
+			<div class="row">						
+				<p class="skill">My Experience</p>
+				<h3>3 Months Exprience</h3><br>
+				<p class="desc" style="padding-bottom: 40px;">I'm a career in programming. My ability is useful for institutions and companies.</p>
+				<div class="span-2">
+					<center><img src="download.png" class="bm"></center><br>
+				</div>
+				<div class="span-8">
+					<h2>Front End Engineer | PT Brainmatics Cipta Informatika</h2>
+					<p>July, 2017 - September, 2017</p><br>
+					<p>Developer team management, technological improvisation, maintenance management, developer leader, and person in charge of digital products are created.</p><br><br>
+				</div>
+			</div>
+		</div>
+	<div class="container">	
+		<div class="row">			
+			<p class="skill">Education</p>
+			<h3>Just My Educational Background</h3><br>
+			<p class="desc" style="padding-bottom:40px;">In addition to mastering the technique, education is also very important to increase the relationship of learning and multiply discussion friends. In addition I also taught ethics.</p>
+		<div class="span-2">
+			<img src="smk.png" class="bm"><br><br>
+		</div>
+		<div class="span-8">
+			<h2>SMK Negeri 1 Rangkasbitung</h2>
+			<h3>Computer and Network Engineering</h3>
+			<h3>2015 - 2018</h3><br>
+			<p>I learned basic computer hardware, networking, programming, and design.</p><br><br>
+		</div>
+		</div>
+	</div>
+</div>
+<div class="span-12">
+	<div class="row">	
+	<div style="background-color: #F9F9F9; padding-top: 40px;">
+		<div class="container">
+			<div class="row">
+				<p class="skill">Portfolio</p>
+				<h3>My Recent Work</h3>
+				<p>Many Project I have done, most of the projects I do alone.</p><br><br>
+				<div class="span-4">
+					<div class="image">
+						<img src="web.jpg" class="bm">
+						<div class="relay">
+							<div class="text">
+								<b>Aplikasi Perpustakaan <br> Daerah </b> <br>
+								<p style="font-size: 16px;"> LKS Banten </p>
+ 							</div>
+						</div>
+					</div><br><br>
+				</div>
+				<div class="span-4">
+					<div class="image">
+						<img src="02.png" class="bm">
+						<div class="relay">
+							<div class="text">
+								<b>TubiArt</b> <br>
+								<p style="font-size: 16px;">Aplikasi Penyewaan Tenda</p> 
+							</div>
+						</div>
+					</div><br><br>
+				</div>
+				<div class="span-4">
+					<div class="image">
+						<img src="web.jpg" class="bm">
+						<div class="relay">
+							<div class="text">
+								Hello
+							</div>
+						</div>
+					</div><br><br>
+				</div>
+			
+			</div>
+			<div class="work">
+				<div class="row">
+					<p class="skill">From Zero To Hero</p>
+					<h3>Reading My Mind</h3>
+					<p>This is the result of my work, hopefully you can read my mind.</p><br><br>
+
+					<div class="span-3">
+						<div class="image">
+							<img src="js.png" class="bm">
+							<br><h3>Javascript <br> Programming</h3>
+						</div><br><br>
+					</div>
+					<div class="span-3">
+						<div class="image">
+							<img src="php.jpeg" class="bm">
+							<br><h3>Native PHP <br> Programming</h3>
+						</div><br><br>
+					</div>
+					<div class="span-3">
+							<div class="image">
+							<img src="layout.png" class="bm">
+						<br><h3>Layout Design</h3>
+						</div><br><br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+<div class="span-12">
+	<div class="container">
+		<div class="row">
+			<p class="skill">Contact Me</p>
+			<h3>Lets Keep In Touch</h3>
+			<p>I'm always available to receive questions about programming or projects.</p><br><br>
+			<div class="contact">
+			<ul>
+				<li>
+					<div class="span-12" style="padding: 40px 0;">
+						<div class="icon ico1">
+							<i class="fa fa-map-signs"></i>
+							</div>
+							<h2>Adreess</h2>
+						<p class="size">Lebak, Banten, Indonesia</p>
+					</div>
+				</li>
+					<hr>
+				<li>
+					<div class="span-12" style="padding: 40px 0;">
+						<div class="icon ico2">
+							<i class="fa fa-phone"></i>
+							</div>
+							<h2>Mobile Number</h2>
+						<p class="size">+6285883680147</p>
+					</div>
+				</li>
+					<hr>
+				<li>
+					<div class="span-12" style="padding: 40px 0;">
+						<div class="icon ico3">
+							<i class="fa fa-envelope"></i>
+							</div>
+							<h2>Email Address</h2>
+						<p class="size">emanuser13@gmail.com</p>
+					</div>
+				</li>
+					<hr>
+					<hr>
+				<li>
+					<div class="span-12" style="padding: 40px 0;">
+						<div class="icon ico4">
+							<i class="fa fa-external-link-alt"></i>
+							</div>
+							<h2>Social Profiles</h2>
+						<ul class="social">
+							<li><a target="_blank" href="https://web.facebook.com/eman.sulaeman13"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a target="_blank" href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a target="_blank" href="https://github.com/maneman7"><i class="fab fa-github"></i></a></li>
+							<li><a target="_blank" href="https://www.linkedin.com/in/maneman7/"><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+				</li>
+				<hr>
+				<hr>
+			</ul>
+			<div class="span-12" style="padding: 20px 0;">
+				<div class="icon peace">
+					<i class="far fa-hand-peace"></i>
+				</div>
+				<h2 style="margin-top:20px;">Declaration</h2><br><br>
+				<p>If you are interested in discussing, hiring or cooperating with me, I am willing to depend on the conditions. I will be ready to receive questions. May I be the one who impressed you.</p>
+				<div class="signature">
+					Eman Sulaeman
+				</div>
+				<div class="span-2">
+					<div class="button">
+						<p><a href="">Download My Resume</a></p>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+
+<div class="row">
+	<div id="footer">
+		<div class="container">
+			<center><p>&copy;2019 Eman Sulaeman. All Rights Reserved</p></center>
+		</div>
+	</div>
+</div>
+</div>
+</main>
+</body>
+</html>
